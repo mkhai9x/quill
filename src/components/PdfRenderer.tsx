667140d650045d933e10448f997c940ccb5b9cc2
Minh Tran <minh.tran.unsw@gmail.com>
2023-10-10 00:09:06 +1100

    add zoom functionality for pdf, this is abit tricky to do

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,5 +1,11 @@
 'use client';
-import { ChevronDown, ChevronUp, Currency, Loader2 } from 'lucide-react';
+import {
+  ChevronDown,
+  ChevronUp,
+  Currency,
+  Loader2,
+  Search,
+} from 'lucide-react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
@@ -15,6 +21,14 @@ import { z } from 'zod';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { cn } from '@/lib/utils';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+
+import SimpleBar from 'simplebar-react';
 
 type PdfRendererProps = {
   url: string;
@@ -23,6 +37,7 @@ type PdfRendererProps = {
 const PdfRenderer = ({ url }: PdfRendererProps) => {
   const [numPages, setNumPages] = useState<number>();
   const [currPage, setCurrPage] = useState<number>(1);
+  const [scale, setScale] = useState<number>(1);
 
   const CustomPageValidator = z.object({
     page: z
@@ -70,7 +85,10 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
           <div className="flex items-center gap-1.5">
             <Input
               {...register('page')}
-              className={cn('w-12 h-8', errors.page && 'focus-visible:ring-red-500')}
+              className={cn(
+                'w-12 h-8',
+                errors.page && 'focus-visible:ring-red-500'
+              )}
               onKeyDown={(e) => {
                 if (e.key === 'Enter') [handleSubmit(handlePageSubmit)()];
               }}
@@ -93,39 +111,67 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
             <ChevronUp className="h-4 w-4" />
           </Button>
         </div>
+
+        <div className="space-x-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button className="g-1.5" aria-label="zoom" variant="ghost">
+                <Search className="h-4 w-4" />
+                {scale * 100}%<ChevronDown className="h-3 w-3 opacity-50" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem onSelect={() => setScale(1)}>
+                100%
+              </DropdownMenuItem>
+              <DropdownMenuItem onSelect={() => setScale(1.5)}>
+                150%
+              </DropdownMenuItem>
+              <DropdownMenuItem onSelect={() => setScale(2)}>
+                200%
+              </DropdownMenuItem>
+              <DropdownMenuItem onSelect={() => setScale(2.5)}>
+                250%
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
 
       {/* PDF display */}
       <div className="flex-1 w-full max-h-screen">
-        <div ref={ref}>
-          <Document
-            loading={
-              <div className="flex justify-center">
-                <Loader2 className="m-24 h-6 w-6 animate-spin" />
-              </div>
-            }
-            onLoadError={() => {
-              toast({
-                title: 'Error Loading pdf',
-                description: 'Please try again later',
-                variant: 'destructive',
-              });
-            }}
-            onLoadSuccess={({ numPages }) => setNumPages(numPages)}
-            file={url}
-            className="max-h-full"
-          >
-            <Page
+        <SimpleBar autoHide={false} className="max-h-[calc(100vh -10rem)]">
+          <div ref={ref}>
+            <Document
               loading={
                 <div className="flex justify-center">
                   <Loader2 className="m-24 h-6 w-6 animate-spin" />
                 </div>
               }
-              pageNumber={currPage}
-              width={width ? width : 1}
-            />
-          </Document>
-        </div>
+              onLoadError={() => {
+                toast({
+                  title: 'Error Loading pdf',
+                  description: 'Please try again later',
+                  variant: 'destructive',
+                });
+              }}
+              onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+              file={url}
+              className="max-h-full"
+            >
+              <Page
+                loading={
+                  <div className="flex justify-center">
+                    <Loader2 className="m-24 h-6 w-6 animate-spin" />
+                  </div>
+                }
+                pageNumber={currPage}
+                scale={scale}
+                width={width ? width : 1}
+              />
+            </Document>
+          </div>
+        </SimpleBar>
       </div>
     </div>
   );
